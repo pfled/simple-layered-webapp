88ca5ea82b20522f29cc0cd3148cffa7cac2c3d6
pfled <connor.pflederer@gmail.com>
2023-01-23 22:08:58 -0500

    Tiny formatting change

--- FactorialWebApp/Program.cs
+++ FactorialWebApp/Program.cs
@@ -3,6 +3,6 @@ using Factorial.Services;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "The factorial of 10 is " + FactorialService.CalculateFactorial(10) +".");
+app.MapGet("/", () => "The factorial of 10 is " + FactorialService.CalculateFactorial(10) + ".");
 
 app.Run();
